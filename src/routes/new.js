3504aa1a0dd1805d385f93fb75de85a1b4bb6e78
Árpád <developedbybarley@gmail.com>
2022-05-08 09:24:30 +0200

    Basic enviroment is created!

--- src/routes/new.js
+++ src/routes/new.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const Recipe = require('../models/addRecipe')
 
 router.get('/', (req, res) => {
-  res.render('new.ejs')
+  res.render('recipes/new.ejs')
 })
 
 router.post('/', async (req, res) => {
-  res.render('new.ejs')
+  res.render('recipes/index.ejs')
 })
 
 module.exports = router;
